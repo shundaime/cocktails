7c9a1b11a24dbf31b8a974bbc46afe1a9812c2c4
shundaime <gcharmassonjm@gmail.com>
2023-07-17 23:51:06 +0200

    update config

--- cocktails-front/vite.config.ts
+++ cocktails-front/vite.config.ts
@@ -7,15 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        vue({
-            template: {
-              compilerOptions: {
-                isCustomElement: (tag) => {
-                  return tag.startsWith('ph-') // (return true)
-                }
-              }
-            }
-          }),
+        vue(),
         vueJsx(),
         VitePWA({
             registerType: 'autoUpdate',
