932253d08075154a76e88b948dbf584605c6eba6
shundaime <gcharmassonjm@gmail.com>
2023-07-18 09:59:33 +0200

    update config and init netlify prod

--- cocktails-front/vite.config.ts
+++ cocktails-front/vite.config.ts
@@ -6,7 +6,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/cocktails-front/',
     plugins: [
         vue(),
         vueJsx(),
